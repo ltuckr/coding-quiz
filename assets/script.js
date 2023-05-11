295123248de87307f20c989e129922f87c2a70c9
ltuckr <l.tucker.rdh@gmail.com>
2023-05-11 12:39:45 -0400

    scrap and retry

--- assets/script.js
+++ assets/script.js
@@ -39,68 +39,10 @@ var questions = [
       c: "onclick"
     },
     correctAnswer: 'c'
-  }
+  },
 ];
 
-function generateQuiz(questions, quizContainer) {
-	var output = [];
-  
-	// for each question...
-	for (var i = 0; i < questions.length; i++) {
-	  var question = questions[i];
-	  var answers = [];
-  
-	  // for each available answer to this question...
-	  for (var letter in question.answers) {
-		answers.push(
-		  '<label>'
-		  + '<input type="radio" name="question' + i + '" value="' + letter + '">'
-		  + letter + ': '
-		  + question.answers[letter]
-		  + '</label>'
-		);
-	  }
-  
-	  output.push(
-		'<div class="question">' + question.question + '</div>'
-		+ '<div class="answers">' + answers.join('') + '</div>'
-	  );
-	}
-  
-	// Combine the output list into one string of HTML and put it on the page
-	quizContainer.innerHTML = output.join('');
-  }
 
-function showResults(questions, quizContainer, resultsContainer) {
-  // Gather user's answers
-  var userAnswers = [];
-
-  var quizInputs = quizContainer.getElementsByTagName('input');
-  for (var i = 0; i < quizInputs.length; i++) {
-    if (quizInputs[i].checked) {
-      userAnswers.push(quizInputs[i].value);
-    }
-  }
-
-  // Calculate score
-  var score = 0;
-  for (var i = 0; i < questions.length; i++) {
-    if (userAnswers[i] === questions[i].correctAnswer) {
-      score++;
-    }
-  }
-
-  // Display the results
-  resultsContainer.innerHTML = 'You scored ' + score + ' out of ' + questions.length;
-}
-
-// Call the generateQuiz function to display the quiz
-generateQuiz(questions, quizContainer);
-
-// Attach the showResults function to the submit button click event
-submitButton.addEventListener('click', function() {
-  showResults(questions, quizContainer, resultsContainer);
-});
 
 
 
