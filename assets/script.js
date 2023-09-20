9cdadc3b1d8ff78bb69ecaeccb6fed04d0519313
Lauren Tucker <l.tucker.rdh@gmail.com>
2023-09-20 18:10:26 -0400

    completed js for next question and end quiz

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,11 @@
-var startButton = document.getElementById("start-btn");
+var startButton = document.getElementById("start"); //changed to "start" button
 var QuizContainer = document.getElementById("quiz-container");
 var questionElement = document.getElementById("question");
 var optionsElement = document.getElementById("options");
 var submitButton = document.getElementById("submit-btn");
 var scoreContainer = document.getElementById("score-container");
 var timerElement = document.getElementById("timer");
+var nextButton = document.getElementById("next"); // Added "Next" button
 
 var currentQuestionIndex = 0;
 var score = 0;
@@ -128,24 +129,80 @@ var questions = [
 // Start button event listener
 startButton.addEventListener("click", startQuiz);
 
-//start quiz function
+// Function to display a question
+function displayQuestion(questionObject) {
+    questionElement.textContent = questionObject.question;
+
+    // Clear previous options
+    optionsElement.innerHTML = "";
+
+    // Loop through answer options and create radio buttons
+    for (var i = 0; i < questionObject.options.length; i++) {
+        var optionLabel = questionObject.options[i];
+        var optionInput = document.createElement("input");
+        optionInput.type = "radio";
+        optionInput.name = "answer";
+        optionInput.value = i;
+
+        var optionText = document.createElement("label");
+        optionText.textContent = optionLabel;
+
+        optionsElement.appendChild(optionInput);
+        optionsElement.appendChild(optionText);
+        optionsElement.appendChild(document.createElement("br"));
+    }
+}
+
+// Function to start the quiz and the timer
 function startQuiz() {
     startButton.classList.add("hide");
     QuizContainer.classList.remove("hide");
-    timerInterval = setInterval(function() {
+
+    // Initialize the timer
+    timerElement.textContent = "Time: " + timeLeft;
+    timerInterval = setInterval(function () {
         timeLeft--;
         timerElement.textContent = "Time: " + timeLeft;
-        if(timeLeft === 0) {
+        if (timeLeft <= 0) {
             clearInterval(timerInterval);
             endQuiz();
         }
     }, 1000);
-    nextQuestion();
+
+    // Display the first question
+    displayQuestion(questions[currentQuestionIndex]);
 }
 
+// Function to move to the next question
+function nextQuestion() {
+    var selectedOption = document.querySelector('input[name="answer"]:checked');
+    if (!selectedOption) {
+        // No option selected, do something (e.g., show a message)
+        return;
+    }
 
+    var selectedAnswer = parseInt(selectedOption.value);
+    if (selectedAnswer === questions[currentQuestionIndex].correctAnswer) {
+        // Correct answer selected, update the score
+        score++;
+    }
 
-  
-  
-  
-  
+    currentQuestionIndex++; // Move to the next question
+    if (currentQuestionIndex < questions.length) {
+        displayQuestion(questions[currentQuestionIndex]); // Display the next question
+    } else {
+        // Handle end of the quiz
+        endQuiz();
+    }
+}
+
+// Add an event listener to the "Next" button
+nextButton.addEventListener("click", nextQuestion);
+
+// Function to end the quiz
+function endQuiz() {
+    clearInterval(timerInterval);
+    QuizContainer.classList.add("hide");
+    scoreContainer.textContent = "Your Score: " + score;
+    scoreContainer.classList.remove("hide");
+}
