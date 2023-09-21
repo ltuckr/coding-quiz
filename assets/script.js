e4995c7b5b38fd34f22acc4e6be6946127f957c6
Lauren Tucker <l.tucker.rdh@gmail.com>
2023-09-21 10:18:59 -0400

    updated options element to choices u1

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
-var startButton = document.getElementById("start"); //changed to "Start" button
+var startButton = document.getElementById("start");
 var QuizContainer = document.getElementById("quiz-container");
 var questionElement = document.getElementById("question");
-var optionsElement = document.getElementById("options");
+var optionsElement = document.getElementById("choicesUl"); // Updated ID
 var submitButton = document.getElementById("submit-btn");
 var scoreContainer = document.getElementById("score-container");
 var timerElement = document.getElementById("timer");
@@ -21,7 +21,7 @@ var questions = [
         "2: <script>",
         "3: <header>"
       ],
-      correctAnswer: '2'
+      correctAnswer: 1 // Updated to match the index of the correct option
     },
     {
       question: "How do you add a comment in JavaScript?",
@@ -30,101 +30,10 @@ var questions = [
         "2: add comment",
         "3: !add comment"
       ],
-      correctAnswer: '1'
+      correctAnswer: 0 // Updated to match the index of the correct option
     },
-    {
-      question: "Java and JavaScript are the same",
-      options: [
-        "1: True",
-        "2: False"
-      ],
-      correctAnswer: '2'
-    },
-    {
-      question: "Which event occurs when the user clicks on an HTML element?",
-      options: [
-        "1: onchange",
-        "2: onpush",
-        "3: onclick"
-      ],
-      correctAnswer: '3'
-    },
-    {
-      question: "What does the acronym 'DOM' stand for in JavaScript?",
-      options: [
-        "Document Order Model",
-        "Document Object Model",
-        "Data Oriented Module",
-        "Digital Output Method"
-      ],
-      correctAnswer: '1'
-    },
-    {
-      question: "Which keyword is used to declare a variable in JavaScript?",
-      options: ["new", "var", "let", "const"],
-      correctAnswer: '1'
-    },
-    {
-      question: "What is the purpose of the 'addEventListener' method in JavaScript?",
-      options: [
-        "To modify the style of an element",
-        "To create a new HTML element",
-        "To add an event handler to an element",
-        "To perform mathematical calculations"
-      ],
-      correctAnswer: '2'
-    },
-    {
-      question: "Which symbol is used for single-line comments in JavaScript?",
-      options: ["//", "##", "--", "/* */"],
-      correctAnswer: '0'
-    },
-    {
-      question: "What is the result of the following expression: '5' + 2?",
-      options: ["7", "52", "3", "'52'"],
-      correctAnswer: '3'
-    },
-    {
-      question: "What is the purpose of the 'setTimeout' function in JavaScript?",
-      options: [
-        "To stop the execution of the script",
-        "To delay the execution of a function",
-        "To perform an asynchronous network request",
-        "To define a loop in the code"
-      ],
-      correctAnswer: '1'
-    },
-    {
-      question: "Which method is used to remove an element from an array in JavaScript?",
-      options: ["erase()", "delete()", "remove()", "splice()"],
-      correctAnswer: '3'
-    },
-    {
-      question: "What does the acronym 'AJAX' stand for in web development?",
-      options: [
-        "Asynchronous JavaScript and XML",
-        "Active JavaScript and XHTML",
-        "Automated JSON and XML",
-        "Advanced Java and XML"
-      ],
-      correctAnswer: '0'
-    },
-    {
-      question: "Which built-in function can be used to convert a string to an integer in JavaScript?",
-      options: ["toInt()", "parseInt()", "convertInt()", "stringToInteger()"],
-      correctAnswer: '1'
-    },
-    {
-      question: "What does the term 'closure' refer to in JavaScript?",
-      options: [
-        "Hiding sensitive information in code",
-        "A type of loop structure",
-        "A method for creating objects",
-        "A function that captures its surrounding state"
-      ],
-      correctAnswer: '3'
-    }
-  ];
+    // ... (other questions)
+];
 
 // Start button event listener
 startButton.addEventListener("click", startQuiz);
